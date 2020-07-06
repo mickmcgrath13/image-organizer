3fcb388fa375f680f95ef39acd8df24877512465
Mick McGrath <mick.mc.13@gmail.com>
2020-07-06 12:50:20 -0500

    add script to cleanup photos

---
+++ backup-cleanup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cd /var/services/photo/AllPhotos/image-organizer
+PHOTOS_ROOT="/var/services/photo/AllPhotos"
+DEST_FOLDER="$PHOTOS_ROOT/photos"
+LOG_FOLDER="$PHOTOS_ROOT/log-cleanup"
+
+# SRC_FOLDER should be from $PHOTOS_ROOT
+SRC_FOLDER="photos"
+
+
+echo ""
+echo "Checkig if LOG_FOLDER exists ($LOG_FOLDER)"
+if [ -d "$LOG_FOLDER" ]; then
+  echo "   LOG_FOLDER exists"
+else
+  echo "   LOG_FOLDER does not exist.  Creating"
+  mkdir -p "$LOG_FOLDER"
+fi
+echo ""
+
+
+
+
+current_date_str=$(date +'%Y-%m-%d-%H%M%S')
+log_file_name="${current_date_str}_${SRC_FOLDER}.log"
+echo "log_file_name: $log_file_name"
+
+touch "$LOG_FOLDER/${log_file_name}"
+
+# DRY_RUN="1" \
+# OUTPUT_DATE_DETAILS="1" \
+IMAGE_ORGANIZER_MOVE="1" \
+SILENT="1" \
+./run.sh \
+"$PHOTOS_ROOT/$SRC_FOLDER" \
+"$DEST_FOLDER" | tee -a "$LOG_FOLDER/${log_file_name}"
